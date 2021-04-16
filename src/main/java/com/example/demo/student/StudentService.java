c99bd068da9d092b110c48c33f740cf6dbc3fd15
MaarBy <64649463+MaarBy@users.noreply.github.com>
2021-04-16 11:33:58 +0200

    Update StudentService.java

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -33,6 +33,12 @@ public class StudentService {
 }
 
 	public void deleteStudent(Long studentId) {
+		boolean exists = studentRepository.existsById(studentId);
+		if(!exists) {
+			throw new IllegalStateException(
+					"student with id " + studentId + " does not exists");
+		}
+		studentRepository.deleteById(studentId);
 		
 	}
 
